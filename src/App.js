c5088f2bf2fca8014e83e3f7e829f619a58db964
boyloe <boyloe@gmail.com>
2020-10-15 08:11:43 -0600

    add css

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ class App extends Component {
 
   filterBagels = (bagels) => {
     let goodBagels = bagels.filter(bagel => bagel.rating !== null)
-    console.log(goodBagels)
     this.setState({bagels: goodBagels})
   }
 
